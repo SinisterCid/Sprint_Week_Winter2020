41fc08ac36850fe6128e7ef2ad82a3352cb7038e
SinisterCid <47873004+SinisterCid@users.noreply.github.com>
2020-03-02 18:45:51 -0500

    Player Script Created
    Basic player script created. Player bounces off the ground and linearly increases bounce force. All adjustable variables remain public for testing and easy access.
    Player moves left and right. Player has its own gravity. Player uses Rigidbody velocity to move

---
+++ Trampoline Bouncer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    public float extraBounceForce;
+    public float baseBounceVelocity;
+
+    public float gravity;
+    public float terminalVelocity;
+
+    public float maxSpeed;
+    public float acceleration;
+    public float deceleration;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        rb.velocity = new Vector2(0, baseBounceVelocity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+        Gravity();
+    }
+
+    void PlayerMovement()
+    {
+        if (Input.GetAxisRaw("Horizontal") == -1 )
+        {
+            rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x - acceleration * Time.deltaTime, -maxSpeed, maxSpeed), rb.velocity.y);
+        }
+        else if (Input.GetAxisRaw("Horizontal") == 1)
+        {
+            rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x + acceleration * Time.deltaTime, -maxSpeed, maxSpeed), rb.velocity.y);
+        }
+        else if (rb.velocity.x < 0)
+        {
+            rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x + deceleration * Time.deltaTime, -maxSpeed, 0), rb.velocity.y);
+        }
+        else if (rb.velocity.x > 0)
+        {
+            rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x - deceleration * Time.deltaTime, 0, maxSpeed), rb.velocity.y);
+        }
+                
+    }
+
+    void Gravity()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y - gravity * Time.deltaTime, -terminalVelocity, Mathf.Infinity));
+    }
+
+    void Bounce()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, baseBounceVelocity + extraBounceForce);
+        baseBounceVelocity = baseBounceVelocity + extraBounceForce;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            Bounce();
+        }
+    }
+}
