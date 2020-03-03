eb2b26274fc6caeee3dab7764d899ec7f11d22a2
Joshua Jewer <joshdtjewer@gmail.com>
2020-03-03 17:42:50 -0500

    Camera

---
+++ Trampoline Bouncer/Assets/Scripts/CameraController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Header("Level Settings")]
+    public GameObject playerTarget;
+    public float levelTopLimit;
+    public float levelBottomLimit;
+
+    [Header("Camera Settings")]
+    public float fromPlayerTopLimit;
+    public float fromPlayerBottomLimit;
+
+    private float FollowPercentage;
+    private Rigidbody2D targetObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetObject = playerTarget.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //How much the player is moving
+
+        FollowPercentage = targetObject.velocity.y;
+
+        //CameraPos = FollowPercentage / FollowSensitivity;
+
+        transform.position = new Vector3(0, Mathf.Clamp(targetObject.transform.position.y + Mathf.Clamp(FollowPercentage, fromPlayerBottomLimit, fromPlayerTopLimit), levelBottomLimit, levelTopLimit), -10);
+    }
+}
