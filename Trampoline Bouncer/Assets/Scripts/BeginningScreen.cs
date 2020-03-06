c7218488f538dedc6725f2d787db49ed5458f5e4
shannonumerez <61715512+shannonumerez@users.noreply.github.com>
2020-03-05 21:07:27 -0500

    :)

---
+++ Trampoline Bouncer/Assets/Scripts/BeginningScreen.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeginningScreen : MonoBehaviour
+{
+
+    public Vector3 input;
+    public float force;
+
+    // Update is called once per frame
+    void Update()
+    {
+        input = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        transform.Translate(-input *force * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(Input.GetKeyDown("Left Ctrl"))
+        {
+            //GameManager.StartButton();
+        }
+    }
+}
